e81f4eef6da46be2cfd6911de9ae524f27d9dda7
michaelgowdy <michaelmichaelgowdy@gmail.com>
2022-11-14 09:47:14 -0600

    Cleaned up unused files and code.

--- AdventureWorks.Web.Blazor/Program.cs
+++ AdventureWorks.Web.Blazor/Program.cs
@@ -23,6 +23,6 @@ namespace AdventureWorks.Web.Blazor
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }); 
     }
 }
